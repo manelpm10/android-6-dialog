6be35d605e21e47bf2209b80cca157e4e71cffde
Manel <yo@manelperez.com>
2018-06-11 11:37:15 +0200

    Attached dialog to activity

--- app/src/main/java/es/pue/android/dialog/MainActivity.java
+++ app/src/main/java/es/pue/android/dialog/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DialogFragment dialog = new DialogFragment();
+        // This preserve status even configuration change (ex, rotate mobile).
+        dialog.setRetainInstance(true);
+        dialog.show(getFragmentManager(), "dialog");
     }
 }
